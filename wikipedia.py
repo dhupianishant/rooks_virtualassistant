45aa9f8c1672ae29430ef84924f9e979adcf3266
Nishant Dhupia <nishantdhupia18@gmail.com>
2022-09-18 09:06:46 +0530

    Delete wikipedia.py

--- wikipedia.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-
-def Wikipedia():
-    speak('Searching Wikipedia...')
-    query = query.replace("wikipedia", "")
-    results = wikipedia.summary(query, sentences=2)
-    speak("According to Wikipedia")
-    print(results)
-    peak(results)
