03a107152abd98ca28e18fc5059a1f62919f22ee
Nishant Dhupia <nishantdhupia18@gmail.com>
2022-09-18 09:07:26 +0530

    Delete location_weather.py

--- location_weather.py
+++
@@ -1,9 +0,0 @@
-def Weather():
-    import geocoder
-    import requests
-    ip = geocoder.ip('me')
-    city = ip.city
-    print(city)
-    url = 'https://wttr.in/{}'.format(city)
-    res = requests.get(url)
-    print(res.text)
